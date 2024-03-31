177ba838d9678bd3988e31041c8356c3d3dd6179
ohta-app <yukihide.github@gmail.com>
2024-04-01 04:35:44 +0900

    コンテンツ作成中

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import './app.scss'
+import './App.scss'
 import Header from './component/Header.tsx'
 import Main from './component/Main.tsx'
 import Aside from './component/Aside.tsx'
 import Footer from './component/Footer.tsx'
-import Nav from "./component/Nav.tsx";
-import {useState} from "react";
+import Nav from './component/Nav.tsx'
+import { useState } from 'react'
 function App() {
-  const [selectedComponent, setSelectedComponent] = useState<string>('');
+  const [selectedComponent, setSelectedComponent] = useState<string>('')
   const handleComponentSelect = (componentId: string) => {
-    setSelectedComponent(componentId);
-  };
+    setSelectedComponent(componentId)
+  }
 
   return (
     <>
@@ -17,10 +17,10 @@ function App() {
         <Header />
       </header>
       <main>
-        <Main selectedComponent={selectedComponent}/>
+        <Main selectedComponent={selectedComponent} />
       </main>
       <nav>
-        <Nav onComponentSelect={handleComponentSelect}/>
+        <Nav onComponentSelect={handleComponentSelect} />
       </nav>
       <aside>
         <Aside />
